a4393654d5d1fbebe01f1fdf4bff0a137142de2d
gogbog <gogbog@abv.bg>
2021-12-04 19:19:10 +0100

    basic movement with sensors

---
+++ Assets/Movement/Scripts/PlayerMovementController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SerialTest;
+    public List<GameObject> laneLocations;
+
+    Rigidbody m_Rigidbody;
+    float m_Speed;
+
+    private Vector3 startingPosition;
+    void Start()
+    {
+        this.startingPosition = this.transform.position;
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 10.0f;
+    }
+
+    void Update()
+    {
+     m_Rigidbody.velocity = transform.forward * m_Speed; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        string input = SerialTest.GetComponent<SerialTest>().message;
+        if (input == null || input == "-1.00" || input == "__Connected__" || input == "")
+        {
+            return;
+        }
+        int index = (int) float.Parse(input);
+        transform.position = new Vector3(laneLocations[index - 1].transform.position.x, transform.position.y, transform.position.z);
+    }
+}
