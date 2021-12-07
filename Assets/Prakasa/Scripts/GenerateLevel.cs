266baeefdd4febd21feb2f9e82c6f546e6bc5849
Alex Petrov <aleks.ppetrov2000@gmail.com>
2021-12-08 00:42:09 +0100

    change prefabs

--- Assets/Prakasa/Scripts/GenerateLevel.cs
+++ Assets/Prakasa/Scripts/GenerateLevel.cs
@@ -16,9 +16,6 @@ public class GenerateLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-
         if (!creatingSections && spawnTimes < 30)
         {
             creatingSections = true;
