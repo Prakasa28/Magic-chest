729ba93e1d1737d90792d8938484a96caa4182bf
Prakasa <i450220@fhict.nl>
2021-12-07 22:53:04 +0100

    Improvised level generation

--- Assets/Prakasa/Scripts/GenerateLevel.cs
+++ Assets/Prakasa/Scripts/GenerateLevel.cs
@@ -6,27 +6,44 @@ public class GenerateLevel : MonoBehaviour
 {
     public GameObject[] section;
     public float xPos = 66;
-    public bool creatingSections = false;
-    public int secNum;
-    int spawnTimes = 30;
- 
+    public int beginningSectionSpawnTimes = 10;
+    public int beginningSectionIndex = 0;
+    private bool creatingSections = false;
+    private int secNum;
+    int spawnTimes = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-       if(!creatingSections && spawnTimes > 0)
+
+
+
+        if (!creatingSections && spawnTimes < 30)
         {
             creatingSections = true;
-            StartCoroutine(GenerateSection());
-            spawnTimes--;
+            if (spawnTimes < beginningSectionSpawnTimes)
+            {
+                StartCoroutine(GenerateSection(beginningSectionIndex));
+            }
+            else
+            {
+                StartCoroutine(GenerateSection());
+            }
+           
+            spawnTimes++;
         } 
     }
-    IEnumerator GenerateSection()
+    IEnumerator GenerateSection(int secNum = -1)
     {
-        secNum = Random.Range(0, 3);
+        if (secNum == -1)
+        {
+            secNum = Random.Range(0, 3);
+        }
+        
         Instantiate(section[secNum], new Vector3(xPos, 4, 0), Quaternion.identity);
         xPos += 66;
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(.1f);
         creatingSections = false;
     } 
 }
