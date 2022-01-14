cb2a26ff92e168c5d24547a66e7a3ebf6122c6b0
Prakasa <i450220@fhict.nl>
2022-01-14 16:44:33 +0100

    Five points text fixed

--- Assets/Prakasa/Scripts/FivePoints.cs
+++ Assets/Prakasa/Scripts/FivePoints.cs
@@ -10,18 +10,38 @@ public class FivePoints : MonoBehaviour
     public TextMeshProUGUI fivePointsText;
     private String fivePoints = "+ 5";
     private bool isFivePointsActive = true;
-
+    private bool textShowed = false;
+    private float collapsed;
+    public float elapsed;
+    
     void OnTriggerEnter(Collider other)
     {
-        
-            foodFx.Play();
-            CollectableControl.scoreCount += 5;
-           // fivePointsText.text = fivePoints.ToString();
-            this.gameObject.SetActive(false);
- 
+
+        collapsed += 10;
+        foodFx.Play();
+        CollectableControl.scoreCount += 5;
+        //this.gameObject.SetActive(false);
+        StartCoroutine(ShowPoints());
     }
 
-    
+    private IEnumerator ShowPoints()
+    {
+        textShowed = true;
+        //make the text visible
+        fivePointsText.text = fivePoints.ToString();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        //gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        //make the text invinsible
+        fivePointsText.text = "";
+        textShowed = false;
+        yield return null;
+
+
+    }
+
+
+
 
 
 
