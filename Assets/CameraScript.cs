3ec03cff0f008294d8c484ac854951f396fe2dd0
440669 <440669@student.fontys.nl>
2022-01-18 17:11:49 +0100

    fixed camera not working in build

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -11,7 +11,8 @@ public class CameraScript : MonoBehaviour
     void Update()
     {
         //Here i assumed that you want to change the X 
-        Vector3 newCamPosition = new Vector3(target.position.x - 8, transform.position.y - 3, transform.position.z );
+        //Vector3 newCamPosition = new Vector3(target.position.x - 8, transform.position.y - 3, transform.position.z );
+        Vector3 newCamPosition = new Vector3(target.position.x - 11, transform.position.y, transform.position.z);
         gameObject.transform.position = newCamPosition;
     }
   }
